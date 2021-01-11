b3128c10f2c32f0129498570f6446a6bfaf1c9aa
Антон М. Ибрагимов <@teplo.local>
2021-01-11 14:40:46 +0500

    prod

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 app = Flask(__name__)
-app.config.from_object('config.DevelopementConfig')
+app.config.from_object('config.ProductionConfig')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_user_page'
